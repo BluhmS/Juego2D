0d1dad6184d438cd716415b0eb2b280b3b41a31c
San7e <sante.bluhm@gmail.com>
2017-08-28 18:47:05 -0300

    first commit

---
+++ Scripts/moves.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moves : StateMachineBehaviour {
+
+	// Use this for initialization
+	private void Awake () {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Animation.SetBool("WalkD", true);
+        }
+	}
+	
+}
